94232e12caa3e40c99b116da2187b6ff21469e9c
Marwan <imaroneu@gmail.com>
2022-09-26 11:44:03 +0700

    commit awal

---
+++ pw/index.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Marwan Hakim
+203040121
+Pertemuan 2
+*/
+?>
+
+<?php
+
+require 'functions.php';
+
+
+// $row = mysqli_fecth_row($result); array numerik
+// $row = mysqli_fecth_assoc($result); array associative
+// $row = mysqli_fecth_array($result); array keduanya
+// tampung ke variabel mahasiswa
+$bk = query("SELECT * FROM buku");
+
+// ketika tombol cari dikembalikan
+if (isset($_POST['cari'])) {
+  $bk = cari($_POST['keyword']);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://unpkg.com/feather-icons"></script>
+  <title>Daftar Koleksi Buku</title>
+</head>
+
+<body>
+
+  <h3 align="center">DAFTAR BUKU</h3>
+
+  <div class="container">
+    <form action="" method="POST">
+      <div class="row mb-4">
+        <div class="col">
+          <div class="form-outline">
+            <br><br>
+            <input type="text" name="keyword" id="form3Example1" class="form-control float-end" placeholder="Cari....."></input><br><br>
+            <button type="submit" class="btn btn-primary btn-block" name="cari">Cari
+              <i data-feather="search" me-2></i>
+            </button>
+            <br>
+            <a type="button" class="btn btn-primary btn-floating float-end" href="tambah.php">Tambah
+              <i data-feather="plus-square" me-2></i>
+            </a>
+          </div>
+        </div>
+    </form>
+  </div>
+
+
+
+
+  <div class="container">
+    <table border="1" cellpadding="10" cellspacing="0" class="table table-hover">
+
+      <tr>
+        <th>No</th>
+        <th>Gambar</th>
+        <th>Judukl Buku</th>
+        <th>Pengarang</th>
+        <th>Tahun Terbit</th>
+        <th>Jumlah Halaman</th>
+        <th>Aksi</th>
+      </tr>
+
+
+      <?php if (empty($bk)) : ?>
+        <tr>
+          <td colspan="4">
+            <p style="color: red; font-style: italic;">data buku tidak ditemukan!</p>
+          </td>
+        </tr>
+      <?php endif; ?>
+
+      <?php $i = 1;
+      foreach ($bk as $b) : ?>
+        <tr>
+          <td class="align-middle"><?= $i++; ?></td>
+          <td class="align-middle"><img src="img/<?= $b['gambar']; ?>" width="120"></td>
+          <td class="align-middle"><?= $b['judul_buku']; ?></td>
+          <td class="align-middle"><?= $b['pengarang']; ?></td>
+          <td class="align-middle"><?= $b['tahun_terbit']; ?></td>
+          <td class="align-middle"><?= $b['jumlah_halaman']; ?></td>
+
+          <td class="align-middle">
+            <a type="button" class="btn btn-primary btn-floating" href="ubah.php?id=<?= $b['id']; ?>">Ubah
+              <i data-feather="edit" me-2></i>
+            </a>
+
+            <a type="button" class="btn btn-danger btn-floating" href="hapus.php?id=<?= $b['id']; ?>">Hapus
+              <i data-feather="trash" me-2></i>
+            </a>
+
+
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
+
+  <script>
+    feather.replace()
+  </script>
+</body>
+
+</html>
---
+++ pw/ubah.php
@@ -0,0 +1,84 @@
+<?php
+
+require 'functions.php';
+
+// jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari url
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM buku WHERE id = $id");
+
+// cek apakah tombol ubah sudah ditekan
+if (isset($_POST['ubah'])) {
+  if (ubah($_POST) > 0) {
+    echo "<script>
+            alert('data berhasil diubah');
+            document.location.href = 'index.php';
+         </script>";
+  } else {
+    echo "data gagal diubah!";
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Ubah Data Buku</title>
+</head>
+
+<body>
+  <h3 align="center">Form Ubah Data Buku</h3>
+  <br>
+  <form action="" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="id" value="<?= $m['id']; ?>">
+
+
+    <div class="container">
+      <div class="form-row">
+        <div class="col">
+          <input type="text" name="judul_buku" class="form-control" placeholder="Judul Buku" autofocus required value="<?= $m['judul_buku']; ?>">
+        </div>
+        <br>
+        <div class="col">
+          <input type="text" name="pengarang" class="form-control" placeholder="Pengarang" required value="<?= $m['pengarang']; ?>">
+        </div>
+        <br>
+        <div class="col">
+          <input type="text" name="tahun_terbit" class="form-control" placeholder="Tahun Terbit" required value="<?= $m['tahun_terbit']; ?>">
+        </div>
+        <br>
+        <div class="col">
+          <input type="text" name="jumlah_halaman" class="form-control" placeholder="Jumlah Halaman" required required value="<?= $m['jumlah_halaman']; ?>">
+        </div>
+        <br>
+      </div>
+
+      <div class="card" style="width:12rem;">
+        <input type="hidden" name="gambar_lama" value="<?= $m['gambar']; ?>">
+        <img src="img/<?= $m['gambar']; ?>" style="display: block;" class="img-preview" class="card-img" alt="...">
+        <div class="card-body">
+          <input type="file" name="gambar" class="gambar" onchange="previewImage()">
+        </div>
+      </div>
+      <div class="d-flex flex-row-reverse">
+        <input class="btn btn-dark float-right" type="submit" value="Ubah" name="ubah">
+      </div>
+
+  </form>
+
+  <script src="script.js"></script>
+
+</body>
+
+</html>
