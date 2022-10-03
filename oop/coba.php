c82b120788e62240cb46e74e53a3793e02bf4b41
Marwan2310 <imaroneu@gmail.com>
2022-10-03 11:44:17 +0700

    video2

---
+++ oop/coba.php
@@ -0,0 +1,17 @@
+<title>Video2</title>
+
+<?php
+class Coba
+{
+}
+
+$a = new COba();
+$b = new COba();
+$c = new COba();
+$d = new COba();
+
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
---
+++ oop/produk.php
@@ -0,0 +1,11 @@
+<?php
+
+//jualan Produk
+
+
+class Produk
+{
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
