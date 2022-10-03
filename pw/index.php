ed2c001313e452946e954435c97fb0f4e0c1b9de
Marwan2310 <imaroneu@gmail.com>
2022-10-03 10:23:13 +0700

    menambah background

--- pw/index.php
+++ pw/index.php
@@ -31,6 +31,7 @@ if (isset($_POST['cari'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <script src="https://unpkg.com/feather-icons"></script>
   <title>Daftar Koleksi Buku</title>
@@ -62,7 +63,7 @@ if (isset($_POST['cari'])) {
 
 
 
-  <div class="container">
+  <div class="container1">
     <table border="1" cellpadding="10" cellspacing="0" class="table table-hover">
 
       <tr>
