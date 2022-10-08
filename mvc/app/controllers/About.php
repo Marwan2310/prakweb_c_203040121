781cba41f95b6ad32d3dc77bd51a1bafa35ea88a
Marwan2310 <imaroneu@gmail.com>
2022-10-08 08:30:02 +0700

    menyelesaikan controller

---
+++ mvc/app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php
+
+class About
+{
+
+  public function index($nama = 'Marwan', $pekerjaan = 'Mahasiswa')
+  {
+    echo "Hallo, nama saya $nama, saya seorang $pekerjaan";
+  }
+
+  public function page()
+  {
+
+    echo 'About/page';
+  }
+}
